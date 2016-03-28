6cf8267193255efc5e1142b973ec7b667527c1e8
PWNAGEP1E <jeremyleemednik@gmail.com>
2016-03-28 15:00:16 -0400

    Tower Defense Version 1

--- Grid/GameScene.swift
+++ Grid/GameScene.swift
@@ -1,6 +1,6 @@
 //
-//  GameScene.swift
-//  Grid
+//  Tower_Defense.swift
+//  
 //
 //  Created by jeremy on 3/28/16.
 //  Copyright (c) 2016 Indicane. All rights reserved.
@@ -9,37 +9,50 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var width: CGFloat!
+    var height: CGFloat = 24
+    var boxSize: CGFloat!
+    var gridStart: CGPoint!
+    
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        createGrid()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
     }
+    
+    func createGrid() {
+        let grid = SKNode() // create a node to hold all grid squares
+        
+        let usableWidth = size.width * 1 // we only want to use 90% of the width available
+        let usableHeight = size.height * 1 // we only want to use 80% of the height available
+        
+        boxSize = usableHeight / height // calculate the box size based on requested height
+        
+        width = CGFloat(Int(usableWidth / boxSize)) // calculate number of boxes along the x axis based on boxSize
+        
+        let offsetX = (size.width - boxSize * width) / 2 // used to center the grid horizontally
+        let offsetY = (size.height - boxSize * height) / 2 // used to center the grid vertically
+        
+        // loop through the entire grid
+        for col in 0 ..< Int(width) {
+            for row in 0 ..< Int(height) {
+                let path = UIBezierPath(rect: CGRect(x: boxSize * CGFloat(col), y: boxSize * CGFloat(row), width: boxSize, height: boxSize))
+                let box = SKShapeNode(path: path.CGPath)
+                box.strokeColor = UIColor.grayColor()
+                box.alpha = 0.3
+                grid.addChild(box)                  }
+        }
+        
+        gridStart = CGPointMake(offsetX, offsetY) // set gridStart for later use
+        grid.position = CGPointMake(offsetX, offsetY) // set the grid position, centered in view
+        addChild(grid) // add the grid to the scene
+    }
+
 }
